4350c221e23b880fdf2afddee018563547a438f8
Shunchao Wang <wang.2799@osu.edu>
2018-05-18 11:03:47 -0400

    Finished routing.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routes/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const appRoot = document.getElementById("app");
 
-ReactDOM.render(<p>This is my react boilerplate</p>, appRoot);
+ReactDOM.render(<AppRouter/>, document.getElementById("app"));
